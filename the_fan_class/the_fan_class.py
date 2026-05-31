0050539825c4e6e45721863dd67b996d40f924f1
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-05-31 19:03:29 +0800

    I created a fan class with a global variable for the speed of the fan and the private instance variables for the constructor with a default values.

---
+++ the_fan_class/the_fan_class.py
@@ -0,0 +1,11 @@
+class Fan:
+
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
+    def __init__(self, speed=SLOW, radius=5, color="blue", on=False):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
