2c7255c74ecd61ad24b9e5ca88bc905168b3f3e0
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-06-03 20:31:14 +0800

    i just added the sound method

--- the_fan_class/fan_tester.py
+++ the_fan_class/fan_tester.py
@@ -57,12 +57,14 @@ class TestFan:
 
         print("Details")
         print("\n\nFan 1")
+        print("Sound:", fan_1.sound())
         print("Speed:", fan_1.get_speed())
         print("Radius:", fan_1.get_radius())
         print("Color:", fan_1.get_color())
         print("On:", fan_1.get_on())
 
         print("\n\nFan 2")
+        print("Sound:", fan_2.sound())
         print("Speed:", fan_2.get_speed())
         print("Radius:", fan_2.get_radius())
         print("Color:", fan_2.get_color())
